2812c8a378c3aeb56cc41e91f43f81a67b845993
elvisrexhaa <elvis2000@hotmail.co.uk>
2025-01-08 14:09:23 +0000

    Initial Commit

---
+++ X&Overse/X_OverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_OverseApp.swift
+//  X&Overse
+//
+//  Created by Elvis Rexha on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct X_OverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ X&OverseTests/X_OverseTests.swift
@@ -0,0 +1,17 @@
+//
+//  X_OverseTests.swift
+//  X&OverseTests
+//
+//  Created by Elvis Rexha on 08/01/2025.
+//
+
+import Testing
+@testable import X_Overse
+
+struct X_OverseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
